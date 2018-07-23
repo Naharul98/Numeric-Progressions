659c75283ca2680ed0cceeafe3a1dc49f8af1eb0
Naharul Hayat <37085164+Naharul98@users.noreply.github.com>
2018-07-23 13:06:34 +0600

    Add files via upload

---
+++ TestProgression2.java
@@ -0,0 +1,19 @@
+ 
+//Class for testing progressions
+class TestProgression2 {
+	public static void main(String[] args) {
+		Progression prog;
+		
+		prog = new ArithProgression(5);
+		prog.printProgression(5);
+		prog.printProgression(7);
+		
+		prog = new GeomProgression(2);
+		prog.printProgression(5);
+		prog.printProgression(7);
+		
+		prog = new FibonacciProgression(3);
+		prog.printProgression(5);
+		prog.printProgression(7);
+	}
+}
